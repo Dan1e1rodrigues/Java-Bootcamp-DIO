2bbf2123cb2866880b5e7d84f7519f11cf30514b
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-08-28 14:50:03 -0300

    Initial commit

---
+++ java-atividade/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int mediaFinal = 4;
+    //Variaveis
+        if(mediaFinal > 6) {
+            System.out.println("APROVADO PARABÊNS");
+        }
+
+        else if (mediaFinal == 6) {
+            System.out.println("QUASE MAIS PASSOU, SORTE ");
+        }
+
+        else{
+            System.out.println("REPROVADO!!! ESTUDE MAIS NA PROXIMA");
+        }
+    }
+}
