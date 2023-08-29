b21313ab15202464a4772f93de33ab0c2d02e604
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-08-28 21:06:37 -0300

    Atualização noturna java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Funcionou Agora!!");
-        //Lançando comentario
-        //Quero que adicione também ao github
+        //Primeiro nome.
+        String primeiroNome ="Daniel";
+        //Segundo nome.
+        String segundoNome ="Fenix Mofl";
+
+        //Metodo para concatenação das duas variaveis.
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        //Impressão ou print para os proximos.
+
+        //Faznedo por concatenação
+        //System.out.println(primeiroNome + segundoNome);
+
+        //Por metodo.
+        System.out.println(nomeCompleto);
     }
+
+    //Metodo que irá conter os valores dos nomes
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+
 }
---
+++ src/tecni.java
@@ -0,0 +1,16 @@
+public class tecni {
+    public static void main(String[] args) {
+        String meuNome = "Daniel";
+
+        int idade = 19;
+
+        double altura = 1.72;
+
+        boolean valor = true;
+
+        System.out.println(meuNome);
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(valor);
+    }
+}
