ed3175d318d3c9147a9a1db29c9247d85c3adaf4
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-09-02 19:34:20 -0300

    Palavras reservadas.

---
+++ palavrasReservadasJava/src/Main.java
@@ -0,0 +1,72 @@
+public class Main {
+    public static void main(String[] args) {
+        //Existem 52 palavras reservadas em java.
+        /*
+        Ordenando em Ordem alfabetica.
+        * abstract
+        * assert
+        * boolean
+        * break
+        * byte
+        * case
+        * catch
+        * char
+        * class
+        * const
+        * continue
+        * default
+        * do
+        * double
+        * else
+        * extends
+        * final
+        * finally
+        * float
+        * for
+        * goto
+        * if
+        * implements
+        * import
+        * instanceof
+        * int
+        * interface
+        * long
+        * native
+        * new
+        * package
+        * private
+        * protected
+        * public
+        * return
+        * short
+        * static
+        * strictfp
+        * super
+        * switch
+        * synchronized
+        * this
+        * throw
+        * throws
+        * transient
+        * try
+        * void
+        * volatil
+        * while
+        *
+        * Outro fator importante e a proteção das classes Por exemplo;
+        *
+        * public: Acesso de qualquer classe.
+        * private: Acesso apenas dentro da classe.
+        * Protected: Acesso por classes no mesmo pacote e subclasses.
+
+        Controle de fluxo dentro de um bloco de código;
+
+        break: sai do bloco de codigo em que ele está.
+
+        case: executa um bloco de código dependendo do teste do switch.
+
+        continue: pula a execução do código que vira após essa linha e vai para proxima passagem do loop.
+
+        * */
+    }
+}
