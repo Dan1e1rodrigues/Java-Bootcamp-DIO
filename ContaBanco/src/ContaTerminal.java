8a651bde68dc83561dfefd7daffc792ff237efb8
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-09-06 20:57:58 -0300

    Atividade conta bancaria.

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,19 @@
+public class ContaTerminal {
+//    public static void main(String[] args){
+//        String nome = args[0];
+//        String sobrenome = args[1];
+//        int idade = Integer.valueOf(args[2]);
+//        double altura = Double.valueOf(args[3]);
+//
+//        System.out.println(nome);
+//        System.out.println(idade);
+//        System.out.println(altura);
+//        System.out.println(sobrenome);
+//    }
+}
+
+/*
+* Sei que a atividade pede para colocar o nome como
+* ContaTerminal e contaBanco porém e bom lembrar
+* que o codigo correto esta na main
+* */
---
+++ ContaBanco/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner saldo = new Scanner(System.in);
+
+        System.out.println("Por favor, Digite seu nome : ");
+        String NomeCliente = saldo.nextLine();
+
+        System.out.println("Por favor, digite o número da sua Agencia !!!: ");
+        int Numero = saldo.nextInt();
+
+        // Consuma a quebra de linha pendente.
+
+        //Fazia com que a linha desse error calculando como vazia.
+        saldo.nextLine();
+
+        //A adição da linha saldo.nextLine(); após a leitura do número
+        // da agência permite que você consuma a quebra de linha pendente,
+        // garantindo que a entrada para a variável Agencia seja lida
+        // corretamente.
+
+        System.out.println("Qual a sua Agencia : ");
+        String Agencia = saldo.nextLine();
+
+        System.out.println("Digite seu saldo : ");
+        double Saldo = saldo.nextDouble();
+
+        System.out.println("Óla " + NomeCliente +  " obrigado por criar uma conta em nosso banco, sua agência é " + Numero + " ,conta " + Agencia + " e seu saldo é " + Saldo + ", já está disponível para saque.");
+    }
+}
