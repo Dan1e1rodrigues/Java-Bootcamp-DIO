f4dbe7494cd266a02078d908ef0d6e8ab15eba38
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-09-12 17:15:37 -0300

    Segundo case dessa vez a proposta era, Foi solicitado, que nosso sistema garanta que, diante das inúmeras candidaturas sejam selecionados apenas no máximo, 5 candidatos para entrevista, onde o salário pretendido seja menor ou igual ao salário base.

--- Controle-de-Candidatos/src/ProcessoSeletivo.java
+++ Controle-de-Candidatos/src/ProcessoSeletivo.java
@@ -2,32 +2,62 @@
 
  * Nesse projeto as etapas vão ser aplicadas em cases de 1 á 5
  * Onde vamos abordar cada capitulo desse processo de criação de
- * projeto.
+ * projeto. Case 1 tratar de condições onde dependendo do valor salaria proposto pelo candidato teremos três tipos de caminhos
  *
  * */
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 
     //Lembrando que posso fazer um metodo para conter valores como no exemplo abaixo
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
+    }
+
+    //
+    static void selecaoCandidatos (){
+        // Array com a lista de candidatos
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+
+        //Buscador de candidatos, nesse while que criei ele irá buscar 5 candidatos contidos
+        //na variavel CANDIDATOS.
+
+        int candidatosSelecionados = 0;
+        int caditadoAtual = 0;
+        double salarioBase= 2000.0;
+        while(candidatosSelecionados > 5 && caditadoAtual < candidatos.length){
+            String candidato = candidatos[caditadoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salario " + salarioPretendido + "");
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            caditadoAtual++;
+        }
+    }
+
+    //Metodo valor pretendido
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.00;
 
-        //
+        //Caso o valor base seja menor que o valor proposto pela empresa.
         if(salarioBase > salarioPretendido) {
             System.out.println("LIGAR PARA O CANDIDATO; ");
         }
 
-        //
+        //Caso o valor do candidato seja igual que o valor proposto pela empresa.
         else if (salarioBase == salarioPretendido) {
             System.out.println("LIGAR PARA O CANDIDATO, COM CONTRA PROPOSTA; ");
         }
 
-        //
+        //Caso o valor solicitado pelo candidato seja maior que o valor proposto
+        //Pela empresa.
         else {
             System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS.");
         }
