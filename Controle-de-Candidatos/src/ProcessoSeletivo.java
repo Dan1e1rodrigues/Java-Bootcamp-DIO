2d451d7347ee33aafaf533dd61d44484135e6282
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-09-13 16:04:04 -0300

    Case 3 e Case 4 finalizados.

--- Controle-de-Candidatos/src/ProcessoSeletivo.java
+++ Controle-de-Candidatos/src/ProcessoSeletivo.java
@@ -5,13 +5,57 @@
  * projeto. Case 1 tratar de condições onde dependendo do valor salaria proposto pelo candidato teremos três tipos de caminhos
  *
  * */
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
-
     //Lembrando que posso fazer um metodo para conter valores como no exemplo abaixo
     public static void main(String[] args) {
-        selecaoCandidatos();
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+        for (String candidato: candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+        static void entrandoEmContato(String candidato) {
+            int tentativasRealizadas = 1;
+            boolean continuarTentando = true;
+            boolean atendeu = false;
+            do {
+                //Elas precisarão sofrerem alteração
+                atendeu = atender();
+                continuarTentando = !atendeu;
+                if (continuarTentando)
+                    tentativasRealizadas++;
+                else
+                    System.out.println("CONTATO REALIZADO COM SUCESSO.");
+
+            }while(continuarTentando && tentativasRealizadas < 3);
+
+                if(atendeu)
+                    System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVAS");
+                else
+                    System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MAXIMO TENTATIVAS " + tentativasRealizadas + " TENTATIVAS");
+        }
+
+        //Metodo auxiliar
+        static boolean atender() {
+            return new Random().nextInt(3)==1;
+        }
+
+    static void imprimirSelecionados() {
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+
+        for(int indice = 0; indice < candidatos.length; indice++){
+            System.out.println("O candidato de n° " + (indice+1) + " é o " + candidatos[indice] );
+        }
+
+        System.out.println("Forma abreviada de interação for each");
+
+        for(String candidato: candidatos){
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
     }
 
     //
